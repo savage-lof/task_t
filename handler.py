9e708d7dedff9072c8eb2f1f15094345bb6a29a6
yulia <lomova20042004@ya.ru>
2021-04-18 21:45:26 +0300

    4я задача

--- handler.py
+++ handler.py
@@ -1,5 +1,9 @@
+from telegram.ext import ConversationHandler
+
 from keyboard import *
 from random import *
+
+flag = False
 
 
 def remove_job_if_exists(name, context):
@@ -16,17 +20,27 @@ def remove_job_if_exists(name, context):
 def task(context):
     """Выводит сообщение"""
     job = context.job
-    context.bot.send_message(job.context, text='Вернулся!')
+    context.bot.send_message(job.context, text='Вернулся!', reply_markup=keyboard_timer())
 
 
 def unset_timer(update, context):
+    global flag
     chat_id = update.message.chat_id
     job_removed = remove_job_if_exists(str(chat_id), context)
     text = 'Хорошо, вернулся сейчас!' if job_removed else 'Нет активного таймера.'
+    update.message.reply_text('таймер сброшен', reply_markup=keyboard_timer())
+    flag = False
 
 
-def set_timer(update, context):
+def timer(update, context):
+    update.message.reply_text('Выберите одну из четырех кнопок', reply_markup=keyboard_timer())
+    return 1
+
+
+def set(update, context):
+    global flag
     times = {'30 секунд': 30, '1 минута': 60, '5 минут': 300}
+    words = {'30 секунд': '30 секунд', '1 минута': '1 минуту', '5 минут': '5 минут'}
     due = times[update.message.text]
 
     chat_id = update.message.chat_id
@@ -35,17 +49,20 @@ def set_timer(update, context):
             str(chat_id), 
             context
         )
+        print(job_removed)
         context.job_queue.run_once(
             task,
             due,
             context=chat_id,
             name=str(chat_id)
         )
-        text = f'Вернусь через {due} секунд!'
+        text = f'засек {words[update.message.text]}'
         if job_removed:
             text += ' Старая задача удалена.'
         # Присылаем сообщение о том, что всё получилось.
-        update.message.reply_text(text, reply_markup=keyboard_close)
+        update.message.reply_text(text, reply_markup=keyboard_close())
+        print(flag)
+        flag = True
 
     except (IndexError, ValueError):
         update.message.reply_text('Использование: /set <секунд>')
@@ -54,7 +71,6 @@ def set_timer(update, context):
 def start(update, context):
     print(4)
     update.message.reply_text('Выберите одну из двух кнопок', reply_markup=keyboard_start())
-    print(5)
 
 
 def dice(update, context):
@@ -73,6 +89,13 @@ def cube(update, context):
         number = [randint(1, 20)]
     line = ', '.join(map(str, number))
     update.message.reply_text(line)
+
+
+def start_2(update, context):
+    if flag:
+        unset_timer(update, context)
+    update.message.reply_text('Выберите одну из двух кнопок', reply_markup=keyboard_start())
+    return ConversationHandler.END
 
     
         
--- main.py
+++ main.py
@@ -7,8 +7,6 @@ TOKEN='1754107976:AAG-gky1bgfhBYaNxj1WV0kyzDmrGp1nudw'
 def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("timer", set_timer))
-
     dp.add_handler(CommandHandler("close", unset_timer, 
                                   pass_chat_data=True))
     print(1)
@@ -20,7 +18,19 @@ def main():
         1: [MessageHandler(Filters.text(['кинуть один шестигранный кубик', 'кинуть 2 шестигранных кубика одновременно',
                                         'кинуть 20-гранный кубик']), cube)]
     },
-    fallbacks=[MessageHandler(Filters.text('вернуться назад'), start)]))
+    fallbacks=[MessageHandler(Filters.text('вернуться назад'), start_2)])
+    )
+    dp.add_handler(ConversationHandler(
+    entry_points=[CommandHandler('timer', timer)],
+    states={
+        1: [MessageHandler(
+            Filters.text(['30 секунд', '1 минута', '5 минут']), set)],
+        2: [CommandHandler("close", unset_timer,
+                                  pass_chat_data=True)]
+    },
+    fallbacks=[MessageHandler(Filters.text('вернуться назад'), start_2)])
+    )
+
 
     print(3)
     updater.start_polling()
